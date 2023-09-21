b3b45fc8ace24c5e35add31a853a3c7ecb5c060e
Ayanara <aya.nathane@gmail.com>
2023-09-21 13:58:53 -0300

    cria o button e seu componente

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
+import {CustomButton} from '../Components';
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -37,7 +38,12 @@ const Home = () => {
               Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
               </p>
 
-            
+              <CustomButton 
+                type="filled"
+                title="Customize It"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
           </motion.div>
         </motion.div>
       </motion.section>
