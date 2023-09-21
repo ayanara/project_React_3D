b63a84e5ec63ebc845956f29a25f1dbc2050bacd
Ayanara <aya.nathane@gmail.com>
2023-09-21 19:09:03 -0300

    salvando alteracoes

--- client/src/Components/CustomButton.jsx
+++ client/src/Components/CustomButton.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const CustomButton = () => {
+import state from '../store';
+
+const generateStyle = (type) => {
+  if(type === 'filled') {
+    return {
+      backgroundColor: '#000',
+      color: '#fff'
+    }
+  }
+}
+
+const CustomButton = ({ type, title, customStyles, handLeClick }) => {
   return (
-    <div>CustomButton</div>
+   <button 
+   className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+   style={generateStyle(type)}
+   >
+    {title}
+   </button>
   )
 }
 
