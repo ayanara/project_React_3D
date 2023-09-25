4ae9b4b8abc30dee5e2c2cb99788dbcedc5b17dd
Ayanara <aya.nathane@gmail.com>
2023-09-25 09:28:35 -0300

    feat: Cria os componentes  CustomButton,   AIPicker,   ColorPicker,   FilerPicker,   Tab, e suas importações no index

---
+++ client/src/Components/FilerPicker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FilerPicker = () => {
+  return (
+    <div>FilerPicker</div>
+  )
+}
+
+export default FilerPicker
--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,3 +1,13 @@
 import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilerPicker from "./FilerPicker";
+import Tab from "./Tab";
 
-export { CustomButton };
+export { 
+  CustomButton,
+  AIPicker,
+  ColorPicker,
+  FilerPicker,
+  Tab,
+ };
