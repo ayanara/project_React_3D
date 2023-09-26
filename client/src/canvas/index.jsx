96c8d120fa202926dff55c83fde6c02372a4a8db
Ayanara <aya.nathane@gmail.com>
2023-09-26 14:23:30 -0300

    Feat: cria o componente Canva que é usado para renderizar cenas 3D

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { Environment, Center } from '@react-three/drei';
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
     <div>Canvas</div>
   )
 }
 
-export default Canvas
+export default CanvasModel
+
+//O componente Canvas é usado para renderizar cenas 3D em React. 
+//O componente Environment fornece um ambiente de iluminação realista para a cena. 
+//O componente Center posiciona a cena no centro da viewport.
+
